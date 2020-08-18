32a88716b1b0300cf1ec1417b91f648362785bfc
Diyama93 <diyamabienvenue@gmail.com>
2020-08-18 23:06:24 +0200

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,5 +1,5 @@
 let hello = function(){
-    console.log('Bonjour comment ça va?')
+    console.log('Bonjour comment ça va??')
 }
 //qu'est ce qui poura etre expoter dans ce fichier avec module.exports
 //module.exports = hello
