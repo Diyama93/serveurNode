5c55392016cf11e4fbe80c5d5d175e65dcfb682f
Diyama93 <diyamabienvenue@gmail.com>
2020-08-18 20:39:28 +0200

    grosse copy
    copier des information volumineuse avec les systeme de streams

--- copy.js
+++ copy.js
@@ -1,11 +1,19 @@
 //créer un script capable de copier un fichier
 let fs = require('fs')
 let file = "demo.mp3"
-let read = fs.createReadStream(file)
-read.on('data', (chunk) =>{
-    console.log("J'ai lu " + chunk.length)
-})
-
-read.on('end',()=>{
-    console.log("J'ai fini de lire le fichier")
+fs.stat(file, (err, stat)=>{    
+    let total = stat.size
+    let progress = 0
+    let read = fs.createReadStream(file)
+    //Les streams
+    let write = fs.createWriteStream('copy.mp3')
+    read.on('data', (chunk) =>{
+        progress += chunk.length
+        console.log("J'ai lu " + Math.round(100 * progress / total) + "%")
+    })
+    //Je veut que tu branche la lecture à l'écriture
+    read.pipe(write)
+    write.on('finish', () => {
+        console.log('Fichier bien copié')
+    })
 })
